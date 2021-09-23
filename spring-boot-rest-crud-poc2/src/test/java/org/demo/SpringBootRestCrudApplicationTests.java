daa77658ebea406a4689c69ca31d7ee8aa65ebba
malleswar-reddy <56329087+malleswar-reddy@users.noreply.github.com>
2021-09-23 18:59:51 +0530

    sample
    test

---
+++ spring-boot-rest-crud-poc2/src/test/java/org/demo/SpringBootRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
+     
